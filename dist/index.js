0814a57b415cfc69a40d2f415761a9feb125b30b
LI Tao <litroncn@gmail.com>
2020-02-01 20:39:03 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -8072,8 +8072,9 @@ async function run() {
     const tagName = core.getInput('tag_name', { required: true });
 
     // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
-    const tag = tagName.replace('refs/tags/', '');
-    const releaseName = core.getInput('release_name', { required: true }).replace('refs/tags/', '');
+    const tag = tagName.toString().replace('refs/tags/', '');
+    const releaseNameTmp = core.getInput('release_name', { required: true });
+    const releaseName = releaseNameTmp.toString().replace('refs/tags/', '');
     const body = core.getInput('body', { required: false });
     const draft = core.getInput('draft', { required: false }) === 'true';
     const prerelease = core.getInput('prerelease', { required: false }) === 'true';
